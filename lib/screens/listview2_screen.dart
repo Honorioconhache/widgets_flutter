07a43a07215c7562a224ef30b3f6f0999eb3463e
Honorio Jaramillo <jaramilloortega@gmail.com>
2023-01-21 12:22:32 -0500

    Trabajando con listview

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const ['Dragon ball', 'Naruto GT', 'One pushman', 'Boruto'];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Listview2'),
+        ),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, i) => ListTile(
+            title: Text(options[i]),
+            trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ));
+  }
+}
